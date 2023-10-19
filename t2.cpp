07e624907031ff576a5eed27547cd64a047f963b
bragalucas1 <lucas.b.moura@ufv.br>
2023-10-19 07:33:09 -0300

    .

--- t2.cpp
+++ t2.cpp
@@ -11,13 +11,18 @@ Trabalho Prático 2 - INF 310 */
 
 using namespace std;
 
-const int chairNumber = 5; //numero de cadeiras na sala de espera
-const int barberChair = 1; //1 cadeira onde o barbeiro corta
-const int clientNumber = 10; //numero arbitrario de clientes
-int clientsWhoEntriedBarberShop = 0;
+const int chairsNumber = 5; //numero de cadeiras na sala de espera
+const int clientsNumber = 10; //numero arbitrario de clientes
+int clientsWhoEntriedBarberShop = 0; //contador inerente a função de parada
 
 class BarberMonitor {
 private:
+    /*Declaração das variáveis a respeito de: 
+    1) mutex para garantir a entrada das threads.
+    2) condição variável para notificar
+    3) numero de clientes que estão esperando na fila para cortar
+    4) booleano a fim de verificar se o barbeiro esta ocioso ou não
+    5) fila para colocar os que estão esperando o corte.*/
     mutex mux;
     condition_variable isSleep;
     int clientsWaiting;
@@ -25,49 +30,43 @@ private:
     queue<int> waitingToCut;
 
 public:
-    BarberMonitor() : clientsWaiting(0), barberSleeping(true) {
-    }
+    BarberMonitor() : clientsWaiting(0), barberSleeping(true) {}
 
     const int getQueueSize() { return waitingToCut.size(); }
 
     void cliente_chega(int id) {
         unique_lock<mutex> lck(mux);
         clientsWhoEntriedBarberShop++;
-        
-        if (clientsWaiting < chairNumber) { /*Se o barbeiro está atendendo um outro cliente, o cliente recém-chegado espera sentado em uma das cadeiras da sala de espera;*/
+
+        if (clientsWaiting < chairsNumber) { /* Se o barbeiro está atendendo outro cliente, o cliente recém-chegado espera sentado em uma das cadeiras da sala de espera; */
             waitingToCut.push(id);
+            cout << "Cliente: " << id << " entrou no salão para ficar na régua.\n";
             clientsWaiting++;
-    
-            /* Se o barbeiro está dormindo, ele o acorda para poder ser atendido;*/
-            if (waitingToCut.size() == 1) {
-                barberSleeping = false;
-                isSleep.notify_one();
-            }
             
-        } else {
-            /*Se todas as cadeiras da sala de espera estão ocupadas, ele desiste (vai embora e não volta mais).*/
-            cout << "Cliente " << id << " desistiu de esperar e procurou outra barbearia." << endl;
+        } else if (clientsWaiting >= chairsNumber) {
+            cout << "Cliente " << id << " cansou de esperar e foi embora." << endl;
+        }
+
+        if (!waitingToCut.empty()) { /*Temos clientes na fila, acorda o dorminhoco.*/
+            barberSleeping = false;
+            isSleep.notify_one();
         }
     }
 
     void barbeiro_faz_corte() {
         unique_lock<mutex> lck(mux);
 
-        cout << "Clients Waiting: " << clientsWaiting << endl;
-        if (clientsWaiting == 0) { /*Ele não deve dormir se houver algum cliente esperando para ser atendido;*/
+        while (clientsWaiting == 0) { 
+            barberSleeping = true;
             cout << "Barbeiro: não há clientes, irei tirar um cochilo. " << endl;
-            isSleep.wait(lck); //e a fila não estiver vazia, isso significa que um cliente chegou e o barbeiro deve acordar para atendê-lo.
+            isSleep.wait(lck); 
         }
         
-        cout << "Opa, chegou cliente!" << endl;
         int id = waitingToCut.front();
         waitingToCut.pop();
         clientsWaiting--;
-        // Barbeiro atende o cliente
         cout << "Barbeiro atendeu o Cliente: " << id << endl;
         cout << "Temos na fila ainda: " << waitingToCut.size() << " clientes." << endl;
-        this_thread::sleep_for(chrono::seconds(4));
-        //lck.unlock();
     }
 };
 
@@ -76,32 +75,33 @@ BarberMonitor barbearia;
 void barbeiro() {
     while (1) {
         barbearia.barbeiro_faz_corte();
-        if(clientsWhoEntriedBarberShop == clientNumber && !(barbearia.getQueueSize())) break; 
+        this_thread::sleep_for(chrono::milliseconds(60));
+        this_thread::yield(); /*Permitir que outras threads entrem durante os cortes.*/
+        if(clientsWhoEntriedBarberShop == clientsNumber && !(barbearia.getQueueSize())) break; 
+        /*Acima, a condição de parada crucial: se o numero de clientes que entraram na barbearia for o número setado de clientes total E 
+        >>não houver mais ninguem na fila para cortar<< encerra o atendimento.*/
     }
-
-    cout << "Barbeiro encerrou o expediente." << endl;
-    
+    cout << "Barbeiro encerrou o expediente. Happy Hour!!!" << endl;
 }
 
 void cliente(int id) {
-    this_thread::sleep_for(chrono::seconds((1/2)));
     barbearia.cliente_chega(id);
 }
 
 int main() {
+    /* Declaração das threads - clientes teremos inúmeras e apenas 1 para o barbeiro.*/
     vector<thread> clientThreads;
     thread barber(barbeiro);
 
-    for (int i = 0; i < clientNumber; i++) {
+    for (int i = 0; i < clientsNumber; i++) {
         thread clientT(cliente, i);
         clientThreads.push_back(move(clientT));
     }
 
-    for (int i = 0; i < clientNumber; i++) {
+    for (int i = 0; i < clientsNumber; i++) {
         clientThreads[i].join();
     }
-    
-    barber.join();
 
+    barber.join();
     return 0;
 }
